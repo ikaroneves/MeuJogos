7eb9c19d37d484052bdf3e2b6a30c15892998c7a
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-18 19:51:45 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/meujogos/ListaJogosActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.meujogos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaJogosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_jogos)
+    }
+}
